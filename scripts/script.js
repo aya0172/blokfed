316bbd8742875d1dd1bfb16ede43be546a147ab6
aya0172 <118114904+aya0172@users.noreply.github.com>
2022-12-01 09:40:51 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+var eersteImagesButton = document.querySelector("header nav li:nth-of-type(4) button");
+
+
+eersteImagesButton.addEventListener("click", eersteImagesButtonKlik);
+
+function eersteImagesButtonKlik() {
+
+  eersteImagesButton.classList.toggle("menuOpen");
+}
